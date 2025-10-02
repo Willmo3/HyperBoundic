653738d253dd4e358272cfe646aa503473e1cce9
willmo3 <thelordofbouge@gmail.com>
2025-10-02 10:29:30 -0400

    Switch real domain to use references

--- src/domains/Real.cpp
+++ src/domains/Real.cpp
@@ -19,31 +19,31 @@ double Real::value() const {
 /*
  * Operations
  */
-Real Real::operator+(Real right) const {
+Real Real::operator+(const Real &right) const {
     return { _value + right._value };
 }
-Real Real::operator-(Real right) const {
+Real Real::operator-(const Real &right) const {
     return { _value - right._value };
 }
-Real Real::operator*(Real right) const {
+Real Real::operator*(const Real &right) const {
     return { _value * right._value };
 }
-Real Real::operator/(Real right) const {
+Real Real::operator/(const Real &right) const {
     return { _value / right._value };
 }
-bool Real::operator==(Real right) const {
+bool Real::operator==(const Real &right) const {
     return _value == right._value;
 }
-bool Real::operator<=(Real right) const {
+bool Real::operator<=(const Real &right) const {
     return _value <= right._value;
 }
-bool Real::operator<(Real right) const {
+bool Real::operator<(const Real &right) const {
     return _value < right._value;
 }
-bool Real::operator>=(Real right) const {
+bool Real::operator>=(const Real &right) const {
     return _value >= right._value;
 }
-bool Real::operator>(Real right) const {
+bool Real::operator>(const Real &right) const {
     return _value > right._value;
 }
 
