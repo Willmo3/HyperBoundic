10ffce3c82c2d432289fe613f25459e91c2421de
willmo3 <thelordofbouge@gmail.com>
2025-10-17 15:47:21 -0400

    Add friedrichs visualizer

--- test/viz/visualize_leapfrog.cpp
+++ test/viz/visualize_leapfrog.cpp
@@ -18,7 +18,6 @@ int main() {
     double delta_t = 0.02; // Spacing of time. Assuming operating over 4 logical time split into 4 timesteps = 1.
 
     auto initial_conditions = std::unique_ptr<Real>(static_cast<Real *>(calloc(discretization_size, sizeof(double))));
-    assert(initial_conditions);
 
     initial_conditions.get()[0] = 1.0;
     initial_conditions.get()[1] = 2.0;
@@ -28,4 +27,6 @@ int main() {
     auto solution_matrix = LeapfrogSolver<Real>::solve(initial_conditions, discretization_size, num_timesteps, delta_t, delta_x, new CubicFlux<Real>());
     solution_matrix.print_system();
     show_real_surface(&solution_matrix);
+
+    return 0;
 }
