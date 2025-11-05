c9e6855a5d84ff4aceccf53beef2acf522aec046
willmo3 <thelordofbouge@gmail.com>
2025-11-05 10:15:32 -0500

    Make sanity test larger

--- src/exe/io/generate_source_files.cpp
+++ src/exe/io/generate_source_files.cpp
@@ -33,32 +33,30 @@ void create_dir() {
 
 // write initial conditions for the domains
 void write_conditions(const std::string& root) {
-    std::vector<Real> real_conds = std::vector<Real>(4);
-    real_conds[0] = 1;
-    real_conds[1] = 2;
-    real_conds[2] = 3;
-    real_conds[3] = 4;
+    auto size = 20;
+    std::vector<Real> real_conds = std::vector<Real>(size);
+
+    for (int i = 0; i < real_conds.size(); i++) {
+        real_conds[i] = i * 0.2 + 1;
+    }
     write_initial_conditions<Real>(root + "/real_conds.json", real_conds);
 
-    std::vector<Winterval> interval_conds = std::vector<Winterval>(4);
-    interval_conds[0] = Winterval(0.9, 1.1);
-    interval_conds[1] = Winterval(1.9, 2.1);
-    interval_conds[2] = Winterval(2.9, 3.1);
-    interval_conds[3] = Winterval(3.9, 4.1);
+    std::vector<Winterval> interval_conds = std::vector<Winterval>(size);
+    for (int i = 0; i < interval_conds.size(); i++) {
+        interval_conds[i] = Winterval(i * 0.2 + 0.9, i * 0.2 + 1.1);
+    }
     write_initial_conditions<Winterval>(root + "/interval_conds.json", interval_conds);
 
-    std::vector<AffineForm> affine_conds = std::vector<AffineForm>(4);
-    affine_conds[0] = AffineForm(Winterval(0.9, 1.1));
-    affine_conds[1] = AffineForm(Winterval(1.9, 2.1));
-    affine_conds[2] = AffineForm(Winterval(2.9, 3.1));
-    affine_conds[3] = AffineForm(Winterval(3.9, 4.1));
+    std::vector<AffineForm> affine_conds = std::vector<AffineForm>(size);
+    for (int i = 0; i < affine_conds.size(); i++) {
+        affine_conds[i] = AffineForm(Winterval(i * 0.2 + 0.9, i * 0.2 + 1.1));
+    }
     write_initial_conditions<AffineForm>(root + "/affine_conds.json", affine_conds);
 
-    std::vector<MixedForm> mixed_conds = std::vector<MixedForm>(4);
-    mixed_conds[0] = MixedForm(Winterval(0.9, 1.1));
-    mixed_conds[1] = MixedForm(Winterval(1.9, 2.1));
-    mixed_conds[2] = MixedForm(Winterval(2.9, 3.1));
-    mixed_conds[3] = MixedForm(Winterval(3.9, 4.1));
+    std::vector<MixedForm> mixed_conds = std::vector<MixedForm>(size);
+    for (int i = 0; i < mixed_conds.size(); i++) {
+        mixed_conds[i] = MixedForm(Winterval(i * 0.2 + 0.9, i * 0.2 + 1.1));
+    }
     write_initial_conditions<MixedForm>(root + "/mixed_conds.json", mixed_conds);
 }
 
@@ -71,7 +69,7 @@ void write_config_files(const std::string &root) {
     for (auto domain: domains) {
         for (auto flux: fluxes) {
             for (auto solver: solvers) {
-                auto cfg = SimulationConfig(domain, flux, solver, 4, 4, 1, 0.01);
+                auto cfg = SimulationConfig(domain, flux, solver, 20, 20, 1, 0.01);
                 auto file_name = root + "/" + domain + "_" + flux + "_" + solver + "_config.json";
                 write_config(file_name, cfg);
             }
